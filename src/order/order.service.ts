61bf30117219ae312c9e96d8502ca10319330adb
amirbena <amir12061968@gmail.com>
2020-11-18 23:34:51 +0200

    start order

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
-
+import { RedisService } from 'nestjs-redis';
+import { Redis } from 'ioredis';
+import { Injectable, Logger } from '@nestjs/common';
+import * as config from 'config';
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    private provider: Redis;
+    private logger = new Logger("UserService");
+
+    constructor(
+        orderProvider: RedisService
+    ) {
+        const dbConfig = config.get("db");
+        const name: string = dbConfig["name"];
+        this.provider = orderProvider.getClient(name);
+    }
+
+}
